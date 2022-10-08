0eb382b380b64a3fdca06e6116fb4c0344b315c2
Benjamin Arntzen <zorlin@gmail.com>
2022-10-09 00:04:49 +0800

    Switch estuary-www to use a wrapper script

---
+++ estuary-www/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [[ -z "${ESTUARY_TOKEN}" ]]; then
+  echo "Loading Estuary token from token file"
+  token_file=/usr/estuary/private/token
+  while [ ! -f $token_file ]
+  do
+    echo token file $token_file not found, sleeping 15s then retrying
+    sleep 15
+  done
+  ESTUARY_TOKEN=$(cat $token_file)
+else
+  echo "An Estuary token was passed as an env var, proceeding"
+
+cd /usr/src/estuary-www
+npm run dev-docker
